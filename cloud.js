08f5631a3d6a29e60dbaae48057674ac179ef010
darksunny025 <125201163@qq.com>
2016-03-31 17:10:39 +0800

    1

--- cloud.js
+++ cloud.js
@@ -4,7 +4,22 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
+	response.success('Hello world!');
+});
+
+/**
+ * 登录
+ */
+AV.Cloud.define('login', function(req, res) {
+	var username = req.param("username");
+	var password = req.param("password");
+	AV.User.logIn(username, password).then(function() {
+		// 成功了，现在可以做其他事情了
+		res.success('登录成功');
+	}, function() {
+		// 失败了
+		res.error('登录失败');
+	});
 });
 
 module.exports = AV.Cloud;
---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+AV.initialize('zDheXlKaohTjVpTde5V5va4W-gzGzoHsz', 'n6UWgFPsrK74nuOOYiwLzcKd');
