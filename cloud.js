c8cf55e4049ade6707e445486d6d2144da7c1b44
darksunny025 <125201163@qq.com>
2016-03-31 17:40:01 +0800

    1

--- cloud.js
+++ cloud.js
@@ -11,8 +11,8 @@ AV.Cloud.define('hello', function(request, response) {
  * 登录
  */
 AV.Cloud.define('login', function(req, res) {
-	var username = req.param("username");
-	var password = req.param("password");
+	var username = req.params.username;
+	var password = req.params.password;
 	AV.User.logIn(username, password).then(function() {
 		// 成功了，现在可以做其他事情了
 		res.success('登录成功');
