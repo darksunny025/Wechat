c5aef29e921bdbf84ef0da42575300c8a6768877
darksunny025 <125201163@qq.com>
2016-04-05 11:52:24 +0800

    1

--- app.js
+++ app.js
@@ -21,15 +21,6 @@ app.use(AV.Cloud.CookieSession({
 	fetchUser: true
 }));
 
-var bodyParser = require('body-parser');
-var multer = require('multer');
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({
-	extended: true
-})); // for parsing application/x-www-form-urlencoded
-app.use(multer()); // for parsing multipart/form-data
-
 /**
  * 登录
  */
